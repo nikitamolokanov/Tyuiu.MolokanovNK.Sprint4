f816d7ae463d1e0e75a920f721c193efefbee5b7
NikitaMolokanov <nikitamolokanov2007@gmail.com>
2025-11-06 21:44:13 +0500

    Task1

---
+++ Tyuiu.MolokanovNK.Sprint4.Task1.V26.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Tyuiu.MolokanovNK.Sprint4.Task1.V26.Lib;
+
+namespace Tyuiu.MolokanovNK.Sprint4.Task1.V26.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+            DataService ds = new DataService();
+
+            int[] numsArray = { 7, 7, 8, 8, 2, 6, 3, 3, 3, 1 };
+            int res = ds.Calculate(numsArray);
+            int wait = 24;
+            Assert.AreEqual(res, wait);
+        }
+    }
+}
---
+++ Tyuiu.MolokanovNK.Sprint4.Task1.V26/Program.cs
@@ -0,0 +1,46 @@
+﻿using Tyuiu.MolokanovNK.Sprint4.Task1.V26.Lib;
+
+DataService ds = new DataService();
+
+Console.Title = "Спринт #3 | Выполнил : Молоканов Н. К. | ИБКСб-25-1";
+//Длина строки 75 символов
+Console.WriteLine("***********************************************************************************************");
+Console.WriteLine("* Спринт #4                                                                                   *");
+Console.WriteLine("* Тема: Базовые навыки работы в C#                                                            *");
+Console.WriteLine("* Задание #1                                                                                  *");
+Console.WriteLine("* Вариант #26                                                                                 *");
+Console.WriteLine("* Выполнил: Молоканов Никита Константинович | ИБКСб-25-1                                      *");
+Console.WriteLine("***********************************************************************************************");
+Console.WriteLine("* УСЛОВИЕ:                                                                                    *");
+Console.WriteLine("* Написать программу используя цикл for, которая вычисляет произведение ряда по формуле       *");
+Console.WriteLine("*                                                                                             *");
+Console.WriteLine("***********************************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                                            *");
+Console.WriteLine("***********************************************************************************************");
+
+int len;
+Console.WriteLine("Введите количество элементов массива");
+len = Convert.ToInt32(Console.ReadLine());
+int[] numsArry= new int[len];
+for (int i = 0; i < len; i++)
+{
+    Console.WriteLine("Введите значение " + i + "элемент массива");
+    numsArry[i] = Convert.ToInt32(Console.ReadLine());
+}  
+Console.WriteLine();
+Console.WriteLine("Массив: ");
+for (int i = 0; i < len; i++)
+{
+    Console.Write(numsArry[i] + "\t");
+}
+Console.WriteLine();
+Console.WriteLine();
+
+Console.WriteLine("***********************************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                                                  *");
+Console.WriteLine("***********************************************************************************************");
+
+int res = ds.Calculate(numsArry);
+
+Console.WriteLine(res);
+Console.ReadLine();
