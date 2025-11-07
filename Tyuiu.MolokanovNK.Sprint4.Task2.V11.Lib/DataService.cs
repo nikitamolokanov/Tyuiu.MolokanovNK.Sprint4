b333d86fc500f36c1c7b31477d34bb519508e04c
NikitaMolokanov <nikitamolokanov2007@gmail.com>
2025-11-07 22:06:08 +0500

    Task2

---
+++ Tyuiu.MolokanovNK.Sprint4.Task2.V11.Lib/DataService.cs
@@ -0,0 +1,20 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+
+namespace Tyuiu.MolokanovNK.Sprint4.Task2.V11.Lib
+{
+    public class DataService : ISprint4Task2V11
+    {
+        public int Calculate(int[] array)
+        {
+            int sum = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] % 2 == 0)
+                {
+                    sum += array[i];
+                }
+            }
+            return sum;
+        }
+    }
+}
---
+++ Tyuiu.MolokanovNK.Sprint4.Task2.V11.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.MolokanovNK.Sprint4.Task2.V11.Lib;
+
+namespace Tyuiu.MolokanovNK.Sprint4.Task2.V11.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+           DataService ds = new DataService();
+
+            int[] numsArray = { 3, 6, 7, 7, 6, 3, 4, 5, 9, 8, 3, 4, 5, 6, 6, 6 };
+            int res = ds.Calculate(numsArray);
+            int wait = 1;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
