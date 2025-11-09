8ee413bf64d61f077bfb8bc31df8417559cfce30
NikitaMolokanov <nikitamolokanov2007@gmail.com>
2025-11-09 23:13:27 +0500

    Task5

---
+++ Tyuiu.MolokanovNK.Sprint4.Task5.V7.Lib/DataService.cs
@@ -0,0 +1,26 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+
+namespace Tyuiu.MolokanovNK.Sprint4.Task5.V7.Lib
+{
+    public class DataService : ISprint4Task5V7
+    {
+        public int Calculate(int[,] matrix)
+        {
+            int rows = matrix.GetLength(0);
+            int cols = matrix.GetLength(1);
+            int count = 0; 
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    if (matrix[i, j] < 0) 
+                    {
+                        count++; 
+                    }
+                }
+            }
+            return count; 
+        }
+    }
+}
---
+++ Tyuiu.MolokanovNK.Sprint4.Task5.V7.Test/DataServiceTest.cs
@@ -0,0 +1,26 @@
+﻿using Tyuiu.MolokanovNK.Sprint4.Task5.V7.Lib;
+
+namespace Tyuiu.MolokanovNK.Sprint4.Task5.V7.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+
+            int[,] numsMatrix =
+                                     { { 2, -2, 5, -5, 9 },
+                                       { -7, 3, 8, 8, -9 },
+                                       { 6, 7, 9, 3, 5 },
+                                       { 7, -5, 7, 7, 8 },
+                                       { 2, 2, -4, 7, 9 } };
+                                                           
+           
+            int res = ds.Calculate(numsMatrix);
+            int wait = 6;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
