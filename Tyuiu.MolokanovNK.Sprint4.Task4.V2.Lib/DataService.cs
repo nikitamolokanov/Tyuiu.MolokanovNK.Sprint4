3245b11dd35e699ab2bc9c5085eba6ce0a979e12
NikitaMolokanov <nikitamolokanov2007@gmail.com>
2025-11-09 22:43:51 +0500

    Task4

---
+++ Tyuiu.MolokanovNK.Sprint4.Task4.V2.Lib/DataService.cs
@@ -0,0 +1,32 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+
+namespace Tyuiu.MolokanovNK.Sprint4.Task4.V2.Lib
+{
+    public class DataService : ISprint4Task4V2
+    {
+        public int[,] Calculate(int[,] matrix)
+        {
+            int rows = matrix.GetLength(0);
+            int cols = matrix.GetLength(1);
+            int[,] res = new int[rows, cols];
+
+            
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    if (matrix[i, j] %2 != 0)
+                    {
+                        res[i, j] = 0;
+                    }
+                    else
+                    {
+                        res[i, j] = matrix[i, j]; 
+                    }
+
+                }
+            }
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.MolokanovNK.Sprint4.Task4.V2/Program.cs
@@ -0,0 +1,64 @@
+﻿using Tyuiu.MolokanovNK.Sprint4.Task4.V2.Lib;
+
+
+DataService ds = new DataService();
+
+Console.Title = "Спринт #3 | Выполнил : Молоканов Н. К. | ИБКСб-25-1";
+//Длина строки 75 символов
+Console.WriteLine("***********************************************************************************************");
+Console.WriteLine("* Спринт #4                                                                                   *");
+Console.WriteLine("* Тема: Базовые навыки работы в C#                                                            *");
+Console.WriteLine("* Задание #1                                                                                  *");
+Console.WriteLine("* Вариант #26                                                                                 *");
+Console.WriteLine("* Выполнил: Молоканов Никита Константинович | ИБКСб-25-1                                      *");
+Console.WriteLine("***********************************************************************************************");
+Console.WriteLine("* УСЛОВИЕ:                                                                                    *");
+Console.WriteLine("* Написать программу используя цикл for, которая вычисляет произведение ряда по формуле       *");
+Console.WriteLine("*                                                                                             *");
+Console.WriteLine("***********************************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                                            *");
+Console.WriteLine("***********************************************************************************************");
+
+Console.Write("Введите количество строк в массиве: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов в массиве: ");
+int cols = Convert.ToInt32(Console.ReadLine());
+
+int[,] matrix = new int[rows, cols];
+Console.WriteLine("***********************************************************************************************");
+
+for(int i = 0; i < rows; i++)
+{
+    for(int j = 0; j < cols; j++)
+    {
+        Console.Write($"Введите {i},{j} элемент массива: ");
+        matrix[i,j] = Convert.ToInt32(Console.ReadLine());
+    }
+}
+Console.WriteLine("\nМассив: ");
+for(int i =0; i < rows; i++)
+{
+    for(int j = 0; j < cols; j++)
+    {
+        Console.Write($"{matrix[i,j]}\t ");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine();
+Console.WriteLine("***********************************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                                                  *");
+Console.WriteLine("***********************************************************************************************");
+
+int[,] res = ds.Calculate(matrix);
+
+Console.WriteLine("Массив после замены нечетных элементов на 0:");
+for (int i = 0; i < rows; i++)
+{
+    for (int j = 0; j < cols; j++)
+    {
+        Console.Write($"{res[i, j]}\t");
+    }
+    Console.WriteLine();
+}
+
+Console.ReadKey();
