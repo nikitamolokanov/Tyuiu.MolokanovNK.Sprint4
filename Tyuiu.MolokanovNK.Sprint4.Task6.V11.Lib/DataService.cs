8701f71cc69cdee78c0df258ae72e23d05111c9e
NikitaMolokanov <nikitamolokanov2007@gmail.com>
2025-11-10 23:46:51 +0500

    Task6

---
+++ Tyuiu.MolokanovNK.Sprint4.Task6.V11.Lib/DataService.cs
@@ -0,0 +1,13 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+
+namespace Tyuiu.MolokanovNK.Sprint4.Task6.V11.Lib
+{
+    public class DataService : ISprint4Task6V11
+    {
+        public int Calculate(string[] array)
+        {
+            string[] mas = Array.FindAll(array, x => x.Length == 5);
+            return mas.Length;
+        }
+    }
+}
---
+++ Tyuiu.MolokanovNK.Sprint4.Task6.V11.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.MolokanovNK.Sprint4.Task6.V11.Lib;
+
+namespace Tyuiu.MolokanovNK.Sprint4.Task6.V11.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            var week = new string[] { "Кошка", "Собака", "Слон", "Жираф", "Бегемот", "Игуана", "Ягуар" };
+            int res = ds.Calculate(week);
+            int wait = 3;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
